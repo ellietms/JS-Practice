b63fae7aa8bc716ffb8e3cff7abc5e9027968933
ellie <ellie.tms1371@gmail.com>
2020-12-17 22:32:35 +0000

    changed the solution for ex-1 , Not completed

--- first-ex.js
+++ first-ex.js
@@ -1,28 +1,33 @@
 // Given an array of strings, find the longest common prefix of any two strings. 
 //  you would return "Hello "
 
+//I'm asking you to find the start of the string which is the same for both strings.
+//The common prefix of any two of [“Hello”, “Hi”, “Cheese”] is “H”
+//The common prefix of every string of [“Hello”, “Hi”, “Cheese”] is the empty string,
+//because they don’t all share one prefix 
+
 let arr = ["Hello Ellie", "Hello Daniel", "I like cheese"];
-let example = ["I am Ellie","Helllooo","heloo ellie","bye ellie","goodbye","goodnight"]
+let example = ["I am Ellie","heloo","heloo ellie","bye ellie","goodbye","goodnight"]
 
 function findLongestPrefix (array) {
-    for(let i = 0 ; i < array.length - 1 ; i++){
-        let firstItem = array[i];
-        let secondItem = array[i+1];
-        let firstPrefix = firstItem.split(" ")[0];
-        let secondPrefix = secondItem.split(" ")[0];
-        let firstPrefixLength = firstPrefix.length;
-        let secondPrefixLength = secondPrefix.length;
-        if(firstPrefixLength > secondPrefixLength) {
-            console.log(firstPrefix);
+    let longestPrefix =[];
+    for(let i=0; i < array.length - 1 ; i++){
+        let anyTwoItems = array.slice(i,i+2);
+        let firstItem = anyTwoItems[0].split("");
+        let secondItem = anyTwoItems[1].split("");
+        let prefix = "";
+        for(let j=0;j<firstItem.length;j++){
+            if(firstItem[j] === secondItem[j]){
+                prefix.concat(firstItem[j])
+            }
         }
-        else{
-            console.log(secondPrefix);
-        }
-       
-        
+        longestPrefix.push(prefix)
     }
+    console.log(longestPrefix);
 }
 
 
-
+findLongestPrefix(example);
+console.log("====")
 findLongestPrefix(arr);
+

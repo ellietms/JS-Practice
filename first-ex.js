426204b2965a8774bb2658cac56b5da45185999f
ellie <ellie.tms1371@gmail.com>
2020-12-18 15:29:09 +0000

    ex-1 Not completed

--- first-ex.js
+++ first-ex.js
@@ -6,24 +6,63 @@
 //The common prefix of every string of [“Hello”, “Hi”, “Cheese”] is the empty string,
 //because they don’t all share one prefix 
 
-let arr = ["Hello Ellie", "Hello Daniel", "I like cheese"];
+let arr = ["Hello Ellie", "Hello Daniel",, "I like cheese"];
 let example = ["I am Ellie","heloo","heloo ellie","bye ellie","goodbye","goodnight"]
 
 function findLongestPrefix (array) {
-    let longestPrefix =[];
-    for(let i=0; i < array.length - 1 ; i++){
-        let anyTwoItems = array.slice(i,i+2);
-        let firstItem = anyTwoItems[0].split("");
-        let secondItem = anyTwoItems[1].split("");
-        let prefix = "";
-        for(let j=0;j<firstItem.length;j++){
-            if(firstItem[j] === secondItem[j]){
-                prefix.concat(firstItem[j])
+    let prefix = [];
+   for(let i=0;i< array.length - 1;i++){
+       //["I am Ellie","heloo"
+    let firstItemLength = array[i].length; //10
+    let secondItemLength = array[i+1].length;//5
+    if(firstItemLength > secondItemLength){
+        for(let j = 0 ; j < secondItemLength ; j++){
+            if(array[i+1].toLowerCase().substring(0,j+1).includes(array[i].toLowerCase().substring(0,j+1))){
+                prefix.push(array[i+1].toLowerCase().substring(0,j+1))
             }
         }
-        longestPrefix.push(prefix)
+        if(prefix.length === 1){
+            array = array.splice(i,i+2).splice(0,0,prefix[0]); 
+            console.log("Array1",array);
+            console.log("prefix1",prefix);
+        }
+        else if(prefix.length !== 0){
+            array = array.splice(i,i+2).splice(0,0, prefix[prefix.length - 1]);
+            console.log("Array2",array);
+            console.log("prefix2",prefix);
+        }
+        else{
+            prefix = []; 
+            array = array.splice(i,i+1)
+            console.log("Array3",array);
+            console.log("prefix3",prefix);
+        }
     }
-    console.log(longestPrefix);
+    else{
+        for(let j = 0 ; j < firstItemLength ; j++){
+            if(array[i].toLowerCase().substring(0,j+1).includes(array[i+1].toLowerCase().substring(0,j+1))){
+                prefix.push(array[i].toLowerCase().substring(0,j+1))
+            }
+        }
+        if(prefix.length === 1){
+            array = array.splice(i,i+2).splice(0,0,prefix[0]); 
+            console.log("2Array1",array);
+            console.log("2prefix1",prefix);
+        }
+        else if(prefix.length !== 0){
+            array = array.splice(i,i+2).splice(0,0,prefix[prefix.length - 1]);
+            console.log("2Array2",array);
+            console.log("2prefix2",prefix);
+        }
+        else{
+            prefix = []; 
+            array = array.splice(i,i+1)
+            console.log("2Array3",array);
+            console.log("2prefix3",prefix);
+        }
+        
+    } 
+   }
 }
 
 

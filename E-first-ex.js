6c951128fa23dc5110c78c0dbc5b6d35da1455b1
ellie <ellie.tms1371@gmail.com>
2020-12-18 21:04:09 +0000

    solved third example

--- E-first-ex.js
+++ E-first-ex.js
@@ -1,2 +1,18 @@
 // Write a function that will check the string is a palindrome or not.
 
+const str = "EllE";
+let newString = []
+
+function palindrome(string){
+    let arrayOfString = string.split("");
+    console.log(arrayOfString);
+    for(let i = 0 ; i < string.length ; i++){
+        if(arrayOfString[i] === arrayOfString[string.length - (i+1)])
+        {
+            newString.push(arrayOfString[string.length - (i+1)])
+        }
+    }
+    console.log("Results,",newString.join(""))
+}
+
+palindrome(str)

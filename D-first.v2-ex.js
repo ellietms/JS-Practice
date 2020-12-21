556f0c7c26c2fd7a4c98c5e306860f380aa6acb7
ellie <ellie.tms1371@gmail.com>
2020-12-21 21:13:21 +0000

    solution of D-ex2 longest prefixes-Long solution :/

--- D-first.v2-ex.js
+++ D-first.v2-ex.js
@@ -1,18 +1,24 @@
 //  how can you compare all of the elements to find the longest common prefix of any two?
 
+let example = [
+    "Helloo Ellie",
+    "I like cheese",
+    "Helloo Daniel",
+  ];
+
 function findCommonPrefixes(array) {
   let item;
   let commonPrefixes = [];
   let index = 0;
   // this for ,  goes through each item in the array
   for (let i = 0; i < array.length; i++) {
-    item = array[i];
+    item = array[i]; 
     // this for goes through the array again
     for (let k = 0; k < array.length - 1; k++) {
       let newExample = example.splice(i, 1);
-      if (k === 0 && commonPrefixes.length === 0) {
+      if (commonPrefixes.length === 0) {
         // this for ,  goes through the length of the item
-        for (let j = 0; j < item.length; j++) {
+        for (let j = 0; j < item.length; j++) {   
           while (item[j] === " ") {
             if (
               newExample[k]
@@ -21,6 +27,9 @@ function findCommonPrefixes(array) {
             ) {
                 commonPrefixes.push(newExample[k].substring(0, j + 1));
             }
+           else{
+               j = item.length;
+           } 
           }
         }
       } 
@@ -35,6 +44,9 @@ function findCommonPrefixes(array) {
             ) {
               commonPrefixes[index].push(newExample[k].substring(0, j + 1));
             }
+            else{
+                j = item.length;
+            } 
           }
         }
       }
@@ -47,22 +59,10 @@ function findCommonPrefixes(array) {
 }
 
 function findLongestCommonPrefix(array) {
-  let prefixes = findCommonPrefixes(array); //it should be an array
-  let longestPrefix;
-  let arrayOfLengths = [];
-  for (let i = 0; i < prefixes.length; i++) {
-     arrayOfLengths.push(prefixes[i].length);
-  }
-  let maxLength = Math.max(...arrayOfLengths);
-  let indexOfLogestWord = arrayOfLengths.indexOf(maxLength);
-  longestPrefix = prefixes[indexOfLogestWord];
-  console.log("longestPrefix", longestPrefix);
+  let prefixes = findCommonPrefixes(array); 
+  let longestPrefix = prefixes.sort((a,b) => b.length - a.length);
+  console.log("longestPrefix", longestPrefix[0]);
 }
 
-let example = [
-  "Helloo Ellie",
-  "I like cheese",
-  "Helloo Daniel",
-];
 
 findLongestCommonPrefix(example);

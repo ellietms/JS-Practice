ab055bd72afb62506ea9968eb7de1e6eeb197567
ellie <ellie.tms1371@gmail.com>
2020-12-23 14:12:58 +0000

    solution of single one

---
+++ E-singleNum.js
@@ -0,0 +1,24 @@
+// given a non-empty array of integers,every element appears twice except for one,find that single one.
+
+//[4,1,2,1,2]
+
+function singleOne(array){
+    let singleOne = [];
+    let length = array.length
+    for(let i=0;i < length;i++){
+        let result = array.some(element => element == array[0])
+        if(result === true){
+            array = array.filter(element => element !== array[0]);
+         }
+         if(array.length === 1 && result === true){ 
+            singleOne.push(array[0]) 
+        }
+    
+}
+console.log("singleOne",singleOne[0])
+console.log("---")
+}
+
+singleOne([1,2,1,2,4]);
+singleOne([2,2,1]);
+singleOne([5,9,5,9,6,7,7,6,2]);

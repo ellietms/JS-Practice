35bad014c7606b8d99139f50164a9e2ad62f183c
ellie <ellie.tms1371@gmail.com>
2020-12-23 13:02:18 +0000

    final solution without checking the equal lengths

--- D1-longestCommonPrefix.js
+++ D1-longestCommonPrefix.js
@@ -1,85 +1,48 @@
-// Given an array of strings, find the longest common prefix of any two strings. 
-// you would return "Hello "
-//I'm asking you to find the start of the string which is the same for both strings.
-//The common prefix of any two of [“Hello”, “Hi”, “Cheese”] is “H”
-//The common prefix of every string of [“Hello”, “Hi”, “Cheese”] is the empty string,
-//because they don’t all share one prefix 
-//  how can you compare all of the elements to find the longest common prefix of any two?
+// can you just write a function which, if you give it two strings, will return the common prefix of the two strings?
 
+//  For function :
+// Initially, the common prefix is empty.
+// Then, we’ll look at each letter, and if they’re the same, we’ll add them to the common prefix.
+// We need to stop when we run out of letters in either word, or when the letters are different.
 
-// Given an array of strings, find the longest common prefix of any two strings. 
-// Goals :
-// Be able to find a common prefix between two strings
-// Be able to find each pair
-// Find the common prefix between each pair
-// Find the longest one
-
-
-
-
-// Steps : 
-//1- try to write your function to print out the pairs
-//to compare every pair of elements, you probably need two loops, one inside the other
-//2- after that :  instead of printing out the pairs, print out the common prefix of each pair
-// 3-Then instead of printing, make an array of them
-// 4-And then find the longest one
-
-
-let example = [
-    "Helloo Ellie",
-    "I like cheese",
-    "Helloo Daniel",
-  ];
-
-  let arr = ["I am Ellie","heloo","heloo ellie","bye ellie","goodbye","goodnight"]
-
-
-
-// step 1 - print Pairs
-function printPairs (array){
-    for(let i = 0 ; i < array.length ; i++){
-        for(let j = 0 ; j < array.length; j++){
-            if( array[i] !==  array[array.length - j] && array[i] !== undefined && array[array.length - j] !== undefined){
-                let pairs = [array[i],array[array.length - j]];
-                // console.log(pairs)
-            }
-        }
+function FindPrefix(str1, str2) {
+  let prefix = "";
+  let lengthStr1 = str1.length;
+  let lengthStr2 = str2.length;
+  let length = Math.min(lengthStr1, lengthStr2);
+  for (let i = 0; i < length; i++) {
+    if (str2.charAt(i) === str1.charAt(i)) {
+      prefix = prefix.concat(str2.charAt(i));
+    } else {
+      break;
     }
+  }
+  return prefix;
 }
 
+FindPrefix("Hello", "Heyloo");
 
-
-//  ---------------------------------------------------------- 
-//step 2 - show prefix
-function FindPrefix(str, str2) {
-    let prefix = "";
-    let lengthStr = str.length;
-    let lengthStr2 = str2.length;
-    let length = Math.min(lengthStr,lengthStr2)
-      for (let i = 0;i<length;i++) {
-        if (str2.charAt(i) === str.charAt(i)) {
-          prefix = prefix.concat(str2.charAt(i));
-        }
-        else{
-          break;
-        }
-      }
-      return (prefix);
-    } 
-// ----------------------------------------------------------------
-
-function printCommonPrefix (array){
-    let finalResults;
-    for(let i = 0 ; i < array.length ; i++){
-        for(let j = 0 ; j < array.length; j++){
-            if( array[i] !==  array[array.length - j] && array[i] !== undefined && array[array.length - j] !== undefined){
-                finalResults = FindPrefix(array[i],array[array.length - j]);
-            }
-        }
+// ["H","HY","L"]
+function commonPrefix(array) {
+  let prefix;
+  let commonPrefixes = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      prefix = FindPrefix(array[i], array[j]);
+      commonPrefixes.push(prefix);
+      console.log("for this pair", array[i],"-" ,array[j]);
+      console.log("common prefix is : ", prefix);
+      console.group("-----");
     }
-    console.log("Longest Prefix in the array is : ",finalResults);
-    console.log("---")
+  }
+  console.log("longest common prefix of this array is : ",commonPrefixes.sort((a,b) => b.length - a.length)[0]);
+  console.log("-----");
 }
 
-// printCommonPrefix(example);
-printCommonPrefix(arr);
+commonPrefix(["I am Ellie","helo","heloo ellie","bye ellie","goodbye","goodnight"]);
+commonPrefix(["Hello", "Hey", "Hi"]);
+commonPrefix([
+  "Helloo Ellie",
+  "I like cheese",
+  "Helloo Daniel",
+])

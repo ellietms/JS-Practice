ac8daea4de86a8b055df721584fa083ed39a1dc9
ellie <ellie.tms1371@gmail.com>
2020-12-24 13:10:27 +0000

    changed the solution from sort to something else

--- D1-longestCommonPrefix.js
+++ D1-longestCommonPrefix.js
@@ -1,5 +1,4 @@
 // can you just write a function which, if you give it two strings, will return the common prefix of the two strings?
-
 //  For function :
 // Initially, the common prefix is empty.
 // Then, we’ll look at each letter, and if they’re the same, we’ll add them to the common prefix.
@@ -20,21 +19,34 @@ function FindPrefix(str1, str2) {
   return prefix;
 }
 
+
 function commonPrefix(array) {
   let prefix;
-  let prefixes = [];
+  let newPrefix = [];
   for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < array.length; j++) {
-      prefix = FindPrefix(array[i], array[j]);
-      if(prefixes.length < prefix.length){
-        prefixes.splice(0,1,prefix)
+      prefix = FindPrefix(array[i], array[j]); //string //He //H
+      if (newPrefix.length === 0) {
+        newPrefix.push(prefix);
+      } else if (newPrefix[newPrefix.length - 1].length < prefix.length) {
+        newPrefix.pop();
+        newPrefix.push(prefix);
+      } else if (newPrefix[newPrefix.length - 1].length === prefix.length) {
+        newPrefix.push(prefix);
+      } else if (newPrefix[newPrefix.length - 1] > prefix.length) {
+        newPrefix = newPrefix;
       }
       console.log("for this pair", array[i], "-", array[j]);
-      console.log("common prefix is : ", prefix);
+      console.log("common prefix is : ", newPrefix);
       console.log("-----");
     }
   }
-  console.log("longestPrefix is",prefixes[0]);
+  if (newPrefix.length > 1) {
+    newPrefix = newPrefix.filter((prefix) => prefix !== "");
+    console.log("longest common prefix is :", newPrefix);
+  } else {
+    console.log("longest common prefix is :", newPrefix[0]);
+  }
 }
 
 commonPrefix([
@@ -45,7 +57,7 @@ commonPrefix([
   "goodbye",
   "goodnight",
 ]);
- commonPrefix(["Hello", "Hey", "Hi"]);
+commonPrefix(["Hello", "Hey", "Hi"]);
 commonPrefix([
   "HellooEllie",
   "I like cheese",

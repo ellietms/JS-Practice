2356ceaeaa0f354c17198cbbf908febb36f5b657
ellie <ellie.tms1371@gmail.com>
2020-12-26 13:56:56 +0000

    add solution of D-longestCommonWord

--- D1-longestCommonPrefix.js
+++ D1-longestCommonPrefix.js
@@ -12,8 +12,7 @@ function FindPrefix(str1, str2) {
   for (let i = 0; i < length; i++) {
     if (str2.charAt(i) === str1.charAt(i)) {
       prefix = prefix.concat(str2.charAt(i));
-    } 
-    else {
+    } else {
       break;
     }
   }
@@ -23,57 +22,68 @@ function FindPrefix(str1, str2) {
 //  Right now you're finding the common prefix between every pair of strings -
 //  can you solve the problem by only needing to compare fewer strings?)
 
-function deleteExtras(array){
+function deleteExtras(array) {
   let firstLetters = [];
   let removedOnes = [];
-  for(let i = 0 ; i < array.length ; i++){
+  for (let i = 0; i < array.length; i++) {
     firstLetters.push(array[i].charAt(0));
   }
-  for(let i = 0 ; i < firstLetters.length ; i++){
-    if(firstLetters.indexOf(firstLetters[i]) === i && firstLetters.indexOf(firstLetters[i],i+1) === -1){
+  let length =  firstLetters.length
+  console.log("without",firstLetters);
+  for (let i = 0; i < length; i++) {
+    console.log(i);
+    if (
+      firstLetters.indexOf(firstLetters[i]) === i &&
+      firstLetters.indexOf(firstLetters[i], i + 1) === -1
+    ) {
       removedOnes.push(firstLetters[i]);
-      firstLetters.splice(i,1)
+      firstLetters.splice(i, 1);
+      i = i - 1;
+      console.log("removed",removedOnes);
+      console.log("with",firstLetters);
     }
   }
-  for(let i=0 ; i< removedOnes.length ; i++){
-    array = array.filter(element => element.charAt(0) !== removedOnes[i])
+  for (let i = 0; i < removedOnes.length; i++) {
+    array = array.filter((element) => element.charAt(0) !== removedOnes[i]);
   }
-  return (array);
+  return array;
 }
-
 
 function commonPrefix(array) {
   let prefix;
   let newPrefix = [];
   let newArray = deleteExtras(array);
-  console.log("newArray",newArray)
-   for (let i = 0; i < newArray.length; i++) {
+  console.log("Final Array without extras : ", newArray);
+  for (let i = 0; i < newArray.length; i++) {
     for (let j = i + 1; j < newArray.length; j++) {
       prefix = FindPrefix(newArray[i], newArray[j]); //string //He //H
       if (newPrefix.length === 0) {
         newPrefix.push(prefix);
-      } else if (newPrefix[newPrefix.length - 1].length < prefix.length) {
+      } else if (newPrefix[newPrefix.length - 1].length < prefix.length && newPrefix.includes(prefix) === false ) {
         newPrefix.pop();
         newPrefix.push(prefix);
-      } else if (newPrefix[newPrefix.length - 1].length === prefix.length) {
+      } else if (newPrefix[newPrefix.length - 1].length === prefix.length && newPrefix[newPrefix.length - 1] !== prefix && newPrefix.includes(prefix) === false ) {
         newPrefix.push(prefix);
       } else if (newPrefix[newPrefix.length - 1] > prefix.length) {
         newPrefix = newPrefix;
       }
+
       console.log("for this pair", newArray[i], "-", newArray[j]);
       console.log("common prefix is : ", newPrefix);
       console.log("-----");
-    
-     }
+    }
   }
-  if (newPrefix.length > 1) {
+  if (newPrefix.length > 1){
     newPrefix = newPrefix.filter((prefix) => prefix !== "");
-    console.log("longest common prefix is :", newPrefix);
-  } else {
-    console.log("longest common prefix is :", newPrefix[0]);
-  }
+    let sortedByLength = newPrefix.sort((a,b) => b.length - a.length);
+    console.log("sortedByLength",sortedByLength);
+    let commonPrefixes = sortedByLength.filter(longestCommons => longestCommons.length === sortedByLength[0].length);
+    console.log("longestCommons",commonPrefixes);
 }
-
+else{
+  console.log("longestCommonPrefix : ",newPrefix);
+}
+}
 commonPrefix([
   "I am Ellie",
   "helo",
@@ -83,7 +93,8 @@ commonPrefix([
   "goodnight",
 ]);
 
-commonPrefix(["Hello", "Hey", "I"]);
+commonPrefix(["Hello", "Hey", "I","Hi"]);
+
  commonPrefix([
   "HellooEllie",
   "I like cheese",
@@ -93,5 +104,4 @@ commonPrefix(["Hello", "Hey", "I"]);
 ]);
 
 
-
-
+commonPrefix(["H","J","HH","JI","JIN","HIJ","K","B","O","Olu","HJIO"])

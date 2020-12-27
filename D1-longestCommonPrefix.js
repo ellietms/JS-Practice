8ef4651b8adda13ade7f9c2b1fa4cfe0c48a43db
ellie <ellie.tms1371@gmail.com>
2020-12-27 17:58:39 +0000

    how to have less pairs for comparing their prefixes

--- D1-longestCommonPrefix.js
+++ D1-longestCommonPrefix.js
@@ -19,59 +19,102 @@ function FindPrefix(str1, str2) {
   return prefix;
 }
 
+// improvement of having less pairs for comparing their prefixes
 //  Right now you're finding the common prefix between every pair of strings -
 //  can you solve the problem by only needing to compare fewer strings?)
-
-
 // I think there is a way you can always not have to compare every element with every other element
-// function deleteExtras(array) {
-  
-  
-// }
+function deleteExtras(array) {
+  let firstLetters = [];
+  let removedOnes = [];
+  for (let i = 0; i < array.length; i++) {
+    firstLetters.push(array[i].charAt(0));
+  }
+  let length =  firstLetters.length;
+  for (let i = 0; i < length; i++) {
+    if (
+      firstLetters.indexOf(firstLetters[i]) === i &&
+      firstLetters.indexOf(firstLetters[i], i + 1) === -1
+    ) {
+      removedOnes.push(firstLetters[i]);
+      firstLetters.splice(i, 1);
+      i = i - 1;
+    }
+  }
+  for (let i = 0; i < removedOnes.length; i++) {
+    array = array.filter((element) => element.charAt(0) !== removedOnes[i]);
+  }
+  return array;
+}
+
 
 function commonPrefix(array) {
   let prefix;
   let newPrefix = [];
-  console.log("Final Array without extras : ", array);
-  for (let i = 0; i < array.length - 1; i++) {
-      prefix = FindPrefix(array[i],array[i + 1]); 
-      //if you know they all have the same length.
-       if (newPrefix.length === 0 || newPrefix[newPrefix.length - 1].length < prefix.length) {
-        newPrefix = [];
-        newPrefix.push(prefix);
-       }
-       else if(newPrefix[newPrefix.length - 1].length === prefix.length && newPrefix.includes(prefix) === false){
-        newPrefix.push(prefix);
-       }
-      
-      console.log("for this pair", array[i], "-", array[i+1]);
-      console.log("common prefix is : ", newPrefix);
-      console.log("-----");
+  array.sort();
+  finalArray = deleteExtras(array);
+  console.log("finalArray",finalArray)
+  for (let i = 0; i < finalArray.length - 1; i++) {
+    prefix = FindPrefix(finalArray[i], finalArray[i + 1]);
+    // improvement of if/else : if you know they all have the same length.
+    if (
+      newPrefix.length === 0 ||
+      newPrefix[newPrefix.length - 1].length < prefix.length
+    ) {
+      newPrefix = [];
+      newPrefix.push(prefix);
+    } else if (
+      newPrefix[newPrefix.length - 1].length === prefix.length &&
+      newPrefix.includes(prefix) === false
+    ) {
+      newPrefix.push(prefix);
+    }
+
+    console.log("for this pair", finalArray[i], "-", finalArray[i + 1]);
+    console.log("common prefix is : ", newPrefix);
+    console.log("-----");
   }
-  // You don't need to sort an array of strings by their length, if you know they all have the same length.
-  console.log("longest Prefix is :",newPrefix);
+  console.log("longest Prefix is :", newPrefix);
 }
 
-commonPrefix(["Hello", "Hey", "Me", "Meep", "Pie", "Pierce"]);
-//  commonPrefix(["Hello", "Hi", "Me", "Meep"]);
-commonPrefix([
-  "I am Ellie",
-  "helo",
-  "heloo ellie",
-  "bye ellie",
-  "goodbye",
-  "goodnight",
-]);
-
-commonPrefix(["Hello", "Hey","Hi"]);
-
- commonPrefix([
-  "HellooEllie",
-  "I like cheese",
-  "HellooDaniel",
-  "Halala",
-  "Halalauu",
-]);
+commonPrefix(["Turkey",
+  "Goodbye",
+  "Hello",
+  "Hi",
+  "Grapes",
+  "Thanks"])
 
 
-// commonPrefix(["H","J","HH","JI","JIN","HIJ","K","B","O","Olu","HJIO"])
+// commonPrefix(["Hello", "Hey", "Me", "Meep", "Pie", "Pierce"]);
+
+// commonPrefix([
+//   "I am Ellie",
+//   "helo",
+//   "heloo ellie",
+//   "bye ellie",
+//   "goodbye",
+//   "goodnight",
+// ]);
+
+// commonPrefix(["Hello", "Hey","Hi"]);
+
+// commonPrefix([
+//   "HellooEllie",
+//   "I like cheese",
+//   "HellooDaniel",
+//   "Halala",
+//   "Halalauu",
+// ]);
+
+// commonPrefix([
+//   "H",
+//   "J",
+//   "HH",
+//   "JI",
+//   "JIN",
+//   "HIJ",
+//   "K",
+//   "B",
+//   "O",
+//   "Olu",
+//   "HJIO",
+// ]);

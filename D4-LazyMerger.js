6d6a6ae2e8fa07d10779a939bb6d9ed0d4e9fb13
ellie <ellie.tms1371@gmail.com>
2020-12-31 17:42:59 +0000

    add 4th problem to start

---
+++ D4-LazyMerger.js
@@ -0,0 +1,5 @@
+// I want you to write a function which takes an array as a parameter.
+//  That array will itself contain some number of arrays.
+//  I want your function to return an object with one method, called next. 
+//  Each time someone calls next, it should return the next element from the next array.
+// If one array is exhausted you should skip over it to the next array.

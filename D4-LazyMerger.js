08e5c47fea61b5ecef6ba333cbcf976e15fdb679
ellie <ellie.tms1371@gmail.com>
2021-01-04 21:29:48 +0000

    first solution

--- D4-LazyMerger.js
+++ D4-LazyMerger.js
@@ -4,57 +4,69 @@
 //  Each time someone calls next, it should return the next element from the next array.
 // If one array is exhausted you should skip over it to the next array.
 
-let example = [
+// let example = [
+//   [1, 2, 3],
+//   ["a", "b", "c"],
+//   [6,7,8,9]
+// ];
+let example = [ 
   [1, 2, 3],
+  [],
   ["a", "b", "c"],
-  [6,7,8,9]
+  [6,7,8,9],
+  [10,11,12],
 ];
 
 function lazyMerger(array) {
   let i = 0;
   let j = 0;
   let element;
-  let lengths = example.map(eachArray => eachArray.length);
-  let maxLength = Math.max(...lengths);
   const result = {
     next: () => {
-      if (i === 0 && j === 0) {
+      // console.log("i is " , i , "j is" , j)
+        if(i === 0 && j === 0){
+          if(array[0][0] !== undefined){
           element = array[0][0];
-         i = i + 1;
-      } 
-      else if (i !== array.length - 1) {
-        if(array[i].length >= j && array[i][j] !== undefined){  
-        element = array[i][j];
-        i  =  i + 1;  
-        } 
-        while(array[i][j] === undefined){
-            i = i + 1;
-            element = array[i][j]
+          i = i + 1;
+          }
         }
-        
+        else if(i !== array.length - 1){
+          if(j <= array[i].length - 1 && array[i].length !== 0){
+            element = array[i][j];
+            i = i + 1;
+          }
+        }
+        else if(i === array.length - 1 && j <= array[i].length - 1 && array[i].length !== 0){
+          if(array[i][j] !== undefined){
+            element = array[i][j];
+            j = j + 1;
+            i = 0;
+          }
+        }
+        else if(i === array.length - 1 && j > array[i].length - 1){
+          element = undefined;
+        }
+        if(array[i][j] === undefined && i !== array.length - 1){
+          while(array[i][j] === undefined && i < array.length - 1){
+          i = i + 1;
+          }
+        }
+        console.log("element =" , element);
+        // console.log("i is " , i , "j is" , j)
+        //  return element;
     }
-      else if (i === array.length - 1) {
-            if(j < maxLength - 1){
-                element = array[i][j]
-                i = 0;
-                j = j + 1;
-            }
-            else if(j === maxLength - 1){
-                element = array[i][j];
-                j = j + 1
-            }
-            if(j === maxLength){
-                element = undefined;
-            }
-           }
-           
-               return element;
-    },
-  };
-  return result;
 }
-
+return result
+}
 const obj = lazyMerger(example);
+console.log(obj.next());
+console.log(obj.next());
+console.log(obj.next());
+console.log(obj.next());
+console.log(obj.next());
+console.log(obj.next());
+console.log(obj.next());
+console.log(obj.next());
 console.log(obj.next());
 console.log(obj.next());
 console.log(obj.next());

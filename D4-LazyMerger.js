5ee2aa70b415fe7becb218a302db8f6a25aac6d7
ellie <ellie.tms1371@gmail.com>
2021-01-05 22:44:26 +0000

    change my solution

--- D4-LazyMerger.js
+++ D4-LazyMerger.js
@@ -4,52 +4,51 @@
 //  Each time someone calls next, it should return the next element from the next array.
 // If one array is exhausted you should skip over it to the next array.
 
-// let example = [
-//   [1, 2, 3],
-//   ["a", "b", "c"],
-//   [6,7,8,9]
-// ];
 let example = [ 
+  [],
   [1, 2, 3],
   [],
   ["a", "b", "c"],
   [6,7,8,9],
+  [],
+  []
 ];
 
 function lazyMerger(array) {
-  let i = 0;
+  let i =0 ;
   let j = 0;
   let element;
   const result = {
     next: () => {
-        if(i === 0 && j === 0){
-          if(array[0][0] !== undefined){
-          element = array[0][0];
-          i = i + 1;
-          }
+      if(array[i][j] !== undefined){
+        if(i < array.length - 1){
+        element = array[i][j];
+        i = i + 1;
         }
-        else if(i !== array.length - 1){
-          if(j <= array[i].length - 1 && array[i].length !== 0){
-            element = array[i][j];
-            i = i + 1;
-          }
+        else if(i === array.length - 1){
+          element = array[i][j];
+          i = 0 ;
+          j = j + 1;
         }
-        else if(i === array.length - 1 && j <= array[i].length - 1 && array[i].length !== 0){
-          if(array[i][j] !== undefined){
-            element = array[i][j];
-            j = j + 1;
-            i = 0;
-          }
+
+      }
+      else if(array[i][j] === undefined){
+        i = i + 1;
+        element = array[i][j];
+        if(array[i][j] !== undefined && i < array.length - 1){
+        i = i + 1;
         }
-        else if(i === array.length - 1 && j > array[i].length - 1){
-          element = undefined;
+        else if(array[i][j] === undefined && i < array.length - 1){
+          i = i + 1 ;
+          element = array[i][j];
+
         }
-        if(array[i][j] === undefined && i !== array.length - 1){
-          while(array[i][j] === undefined && i < array.length - 1){
-          i = i + 1;
-          }
+        else if(array[i][j] === undefined  && i === array.length - 1){
+          i = 0;
+          j = j + 1;
         }
-          return element;
+      }
+      return element;
     }
 }
 return result

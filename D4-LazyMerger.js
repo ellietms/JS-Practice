4be4be7c6b43baa9090bb4a3bda902d4a25125fa
ellie <ellie.tms1371@gmail.com>
2021-01-12 19:30:01 +0000

    my solution with undefinedgit add .

--- D4-LazyMerger.js
+++ D4-LazyMerger.js
@@ -4,42 +4,45 @@
 //  Each time someone calls next, it should return the next element from the next array.
 // If one array is exhausted you should skip over it to the next array.
 
-
 // Find the next non-undefined element" is a single operation, which you don't do the same each time
 //  And you don't look arbitrarily far ahead in the array,
 //  you always look either one or two elements forward, rather than keeping looking until you find one
 // So... Work out the general way of finding the next element, and write that, maybe as a method of its own
 
-
-let example = [ 
-  [1, 2, 3],
-  [],
-  ["a", "b", "c"],
-  [6,7,8,9]
-];
+let example = [[1, 2, 3], [], ["a", "b", "c"], [6, 7, 8, 9]];
 function lazyMerger(array) {
-  let firstIndex = 0;
-  let secondIndex = 0;
+  let rowIndex = 0;
+  let columnIndex = 0;
   let element;
   const result = {
     next: () => {
-      while(array[firstIndex][secondIndex] === undefined)
-      {
-        firstIndex = firstIndex + 1;
+      let checked = 0;
+      while (checked <= columnIndex) {
+        if(array[rowIndex][columnIndex] !== undefined ){
+          element = array[rowIndex][columnIndex];
+          rowIndex = (rowIndex + 1) % array.length;
+          if(rowIndex === 0){
+            columnIndex++;
+          }
+          return element;
+        }
+        else{
+          while(array[rowIndex][columnIndex] === undefined){
+            rowIndex = (rowIndex + 1) % array.length;
+            if(rowIndex === 0){
+              columnIndex++;
+            }
+          }
+        }
+        checked++;
       }
-      if(array[firstIndex][secondIndex] !== undefined){
-        element = array[firstIndex][secondIndex];
-      }
-      if(firstIndex === array.length - 1){
-        firstIndex = -1;
-        secondIndex = secondIndex + 1;
-      }
-      return element;
+      return undefined;
+    }
 }
-  }
-return result
+  return result;
 }
 const obj = lazyMerger(example);
+console.log(obj.next());
 console.log(obj.next());
 console.log(obj.next());
 console.log(obj.next());

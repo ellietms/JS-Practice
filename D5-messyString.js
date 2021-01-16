96d2549a7d49f3e9ef0403798b4659933216b070
ellie <ellie.tms1371@gmail.com>
2021-01-16 20:46:29 +0000

    not-completed solution day1

--- D5-messyString.js
+++ D5-messyString.js
@@ -1,10 +1,10 @@
-
 // I want you to write a function which accepts a string as a parameter, and returns a new string
 // The new string should be the same as the old one, but all of the upper case letters first,
 // then all of the lower case letters, in the order that they occurred in the original string
 
 let example ="Hello Ellie! How are you today? I like CHEESE"
 // HEHICHEESEello llie! ow are you today?  like 
+
 
 function messyString(string){
     let i = 0;
@@ -14,6 +14,7 @@ function messyString(string){
         if(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90){
             allUpperCases.push(string.slice(i,i+1));
             string = string.slice(i+1);
+            console.log("New String",string)
             i = 0;
         }
         else{

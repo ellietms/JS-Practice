9df448db4599bc1413be5cae44a53d671bd7c0f4
ellie <ellie.tms1371@gmail.com>
2021-02-02 20:00:08 +0000

    added second solution- faster one

--- Big-O/Timing-Our-Code.js
+++ Big-O/Timing-Our-Code.js
@@ -1,6 +1,10 @@
 // write a function that calculates the sum of all numbers from 1 up to some number(n)
 
+// differences between different solutions : 
 
+https://rithmschool.github.io/function-timer-demo/
+
+// my solution
 function sum(n){
     let sum = 0;
     for(let i = 1 ; i <= n ; i++){
@@ -9,7 +13,20 @@ function sum(n){
     return(sum);
 }
 
-console.log(sum(2));
-console.log(sum(3));
-console.log(sum(4));
-console.log(sum(10));
+
+// Better solution
+function sumBetter(n){
+    return n * (n+1) / 2
+}
+
+
+
+// let time1 = performance.now();
+console.log(sum(200));
+// let time2 = performance.now();
+// console.log(`time for my solution ${time2 - time1}`)
+
+// let time3 = performance.now();
+console.log(sumBetter(200));
+// let time4 = performance.now();
+// console.log(`time Better Solution ${time4 - time3}`);

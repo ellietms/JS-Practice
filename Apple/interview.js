84bd619893b2c6fce85dd80aa190f5c7136adbe5
ellie <ellie.tms1371@gmail.com>
2021-02-09 22:33:18 +0000

    add new solution

--- Apple/interview.js
+++ Apple/interview.js
@@ -3,25 +3,21 @@ let storage = [];
 
 function store(rangesOfIntegers, colour) {
   if (colour === "YELLOW") {
-      priority = 1;
-   }
-  else if(colour === "RED"){
-      priority = 2;
-   }
-  else if (colour === "GREEN"){
+    priority = 1;
+  } else if (colour === "RED") {
+    priority = 2;
+  } else if (colour === "GREEN") {
     priority = 3;
-  } 
-  else if (colour === "BLUE"){
+  } else if (colour === "BLUE") {
     priority = 4;
-  } 
-  else if (colour === "GREY"){
+  } else if (colour === "GREY") {
     priority = 5;
   }
   storage.push({
     lowerBound: Number(rangesOfIntegers.slice(0, 2)),
     upperBound: Number(rangesOfIntegers.slice(3, 5)),
     colour: colour,
-    priority: priority
+    priority: priority,
   });
 }
 
@@ -31,15 +27,15 @@ function get(stringShowsInteger) {
       eachObject.lowerBound <= Number(stringShowsInteger) &&
       Number(stringShowsInteger) <= eachObject.upperBound
   );
-   if (filteredStorageForSpecificInteger.length >= 1) {
+  if (filteredStorageForSpecificInteger.length >= 1) {
     filteredStorageForSpecificInteger.sort(
-      (firstObject, secondObject) => firstObject.priority - secondObject.priority
+      (firstObject, secondObject) =>
+        firstObject.priority - secondObject.priority
     );
-    return filteredStorageForSpecificInteger[0].colour
-   }
-   else if (filteredStorageForSpecificInteger.length === 0) {
-     return "GRAY";
-   }
+    return filteredStorageForSpecificInteger[0].colour;
+  } else if (filteredStorageForSpecificInteger.length === 0) {
+    return "GRAY";
+  }
 }
 
 // Extra Idea :
@@ -52,9 +48,9 @@ console.log("Apple Example Results :");
 store("34-78", "RED");
 store("31-41", "YELLOW");
 store("64-98", "GREEN");
- console.log(get("31"));
- console.log(get("39"));
- console.log(get("50"));
+console.log(get("31"));
+console.log(get("39"));
+console.log(get("50"));
 console.log(get("68"));
 console.log(get("91"));
 console.log(get("99"));

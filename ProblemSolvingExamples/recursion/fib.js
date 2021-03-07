682356d9400e9da3b4ff9165d84af6affe3f8ee5
ellie <ellie.tms1371@gmail.com>
2021-03-07 18:22:16 +0000

    add fibonacci sequence solution

--- ProblemSolvingExamples/recursion/fib.js
+++ ProblemSolvingExamples/recursion/fib.js
@@ -4,11 +4,14 @@ function fib(number) {
   if (number === 1 || number === 2) {
     return 1;
   }
-  return fib(number - 1) + fib(number - 2);
+  if(number === 0){
+      return 0;
+  }
+  return fib(number - 1) + fib(number - 2); 
 }
 
-console.log(fib(4)); // 3      1+1 +2+3   f(3) => f(1) + f(0) f(4) => f(3) + f(2)  1
-// f(2) + f(1)
+console.log(fib(0));
+console.log(fib(4)); // 3     
 console.log(fib(10)); // 55
 console.log(fib(28)); // 317811
 console.log(fib(35)); // 9227465

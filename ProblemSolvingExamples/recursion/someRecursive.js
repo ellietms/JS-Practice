58af494145e785ff5d105a99a5d0367b375897b2
ellie <ellie.tms1371@gmail.com>
2021-03-08 21:47:52 +0000

    add solution for someRecursive function

--- ProblemSolvingExamples/recursion/someRecursive.js
+++ ProblemSolvingExamples/recursion/someRecursive.js
@@ -1,6 +1,26 @@
-
-function someRecursive(array,callback){
- 
- 
- 
+function someRecursive(array, ourFunction) {
+  let result;
+  function helper(array) {
+    if (array.length !== 0) {
+      if (ourFunction(array[0]) === true) {
+        result = true;
+        return result;
+      } 
+      else {
+        array = array.slice(1);
+        result = false;
+        helper(array);
+      }
+    }
+    return result;
+  }
+  helper(array);
+  return result;
 }
+
+const isOdd = (val) => val % 2 !== 0;
+
+console.log(someRecursive([1, 2, 3, 4], isOdd)); // true
+console.log(someRecursive([4, 6, 8, 9], isOdd)); // true
+console.log(someRecursive([4, 6, 8], isOdd)); // false
+console.log(someRecursive([4, 6, 8], (val) => val > 10)); // false

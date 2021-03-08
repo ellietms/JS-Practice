a09906e2d199eb6ce82c2cec5a2908613e9d00a4
ellie <ellie.tms1371@gmail.com>
2021-03-08 08:25:55 +0000

    add solution for isPalindrome

--- ProblemSolvingExamples/recursion/isPalindrome.js
+++ ProblemSolvingExamples/recursion/isPalindrome.js
@@ -1,14 +1,31 @@
+function reverse(string) {
+    let words = "";
+    function helper(string) {
+      if (string.length !== 0) {
+        let newWord;
+        newWord = string[string.length - 1];
+        string = string.slice(0, string.length - 1);
+        words = words.concat(newWord);
+        helper(string);
+      }
+    }
+    helper(string);
+    return words;
+  }
+
 
 function isPalindrome(string){
-  
-  
-  
+    let reverseString  = reverse(string);
+    if(reverseString === string){
+        return true;
+    }
+  return false;
 }
 
 
 
-// isPalindrome('awesome') // false
-// isPalindrome('foobar') // false
-// isPalindrome('tacocat') // true
-// isPalindrome('amanaplanacanalpanama') // true
-// isPalindrome('amanaplanacanalpandemonium') // false
+console.log(isPalindrome('awesome')) // false
+console.log(isPalindrome('foobar')) // false
+console.log(isPalindrome('tacocat')) // true
+console.log(isPalindrome('amanaplanacanalpanama')) // true
+console.log(isPalindrome('amanaplanacanalpandemonium')) // false

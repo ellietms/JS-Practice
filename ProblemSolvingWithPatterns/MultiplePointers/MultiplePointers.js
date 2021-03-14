a6c554ddd852040788564e8ea9efc24118d55f08
ellie <ellie.tms1371@gmail.com>
2021-03-14 10:55:01 +0000

    add averagePair solution with multiplePointer

--- ProblemSolvingWithPatterns/MultiplePointers/MultiplePointers.js
+++ ProblemSolvingWithPatterns/MultiplePointers/MultiplePointers.js
@@ -1,29 +1,29 @@
-function averagePair(array,target){
-    if(array.length === 0){
+
+function averagePair(sortedArray,targetAverage){
+    if(sortedArray.length === 0){
         return false;
     }
     let currentIndex = 0;
-    for(let pointer = 1; pointer <= array.length - 1;pointer++){
-        console.log("pointer",pointer);
-        console.log("currentIndex",currentIndex);
-        let average = (array[currentIndex] + array[pointer]) / 2;
-        console.log("array[currentIndex] = ",array[currentIndex] , "array[pointer]" , array[pointer])
-        console.log("average",average);
-        console.log("=========")
-        if(average === target){
+    for(let pointer = currentIndex + 1 ; pointer <= sortedArray.length - 1;pointer++){
+        let average = (sortedArray[currentIndex] + sortedArray[pointer]) / 2;
+        if(average === targetAverage){
             return true;
         }
-        else if(pointer === array.length - 1){
-            currentIndex += 1;
-            pointer = currentIndex + 1
+        else if(pointer === sortedArray.length - 1){
+          currentIndex += 1;
+           pointer = 1;
         }
-        else if(currentIndex )
+        else if(currentIndex === sortedArray.length - 1){
+            return false;
+        }
+        
     } 
     return false; 
   }
 
 
-  console.log(averagePair([1,2,3],2.5));
-  averagePair([1,3,3,5,6,7,10,12,19],8);
-  averagePair([-1,0,3,4,5,6],2.5);
-  averagePair([],4);
+  console.log(averagePair([1,2,3],2.5)); //true
+  console.log(averagePair([1,3,3,5,6,7,10,12,19],8));//true
+  console.log(averagePair([],4));//false
+  console.log(averagePair([-1,0,3,4,5,6],4.1));//false
+  

39e7e2e38a1c419d67222023be6df983cd28c6b6
ellie <ellie.tms1371@gmail.com>
2021-03-16 19:42:55 +0000

    add slide window explanation

---
+++ ProblemSolvingWithPatterns/slidingWindow/maxSubArraysum.js
@@ -0,0 +1,11 @@
+
+function maxSubarraySum(array,sumLength){
+    
+}
+
+
+console.log(maxSubarraySum([100,200,300,400],2)); //700
+console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20],4)); //39
+console.log(maxSubarraySum([-3,4,0,-2,6,-1],2)); //5
+console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2)); //5
+console.log(maxSubarraySum([2,3],3)); //null

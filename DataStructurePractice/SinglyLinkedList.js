d07373c087ec0635dddd8cc0f7c996c4d2d7cf9a
ellie <ellie.tms1371@gmail.com>
2021-03-23 20:48:22 +0000

    add

--- DataStructurePractice/SinglyLinkedList.js
+++ DataStructurePractice/SinglyLinkedList.js
@@ -1,20 +1,27 @@
 
 
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
 class SinglyLinkedList{
     constructor(){
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
-    push(newNode){
-        if(this.head === null){
-            head = newNode;
-            tail = newNode;
-            length = 1;
+    push(newValue){
+        if(!(this.head)){
+           this.head = new Node(newValue);
+           this.tail = head;
         }
         else{
-            tail = newNode;
-            length += 1;
+           this.tail = new Node(newValue);
+           this.tail.next = 
         }
     }
 

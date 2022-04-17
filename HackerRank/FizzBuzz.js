9a02c77f7359d51b3915efeabbcfe96dab831bec
Ellie <ela@Elas-MacBook-Air.local>
2022-04-17 20:51:52 +0100

    added code for practicing JS

---
+++ HackerRank/FizzBuzz.js
@@ -0,0 +1,20 @@
+function fizzBuzz(number) {
+    let i = 1
+    while(i < number + 1){
+        if(i % 15 === 0){
+            console.log("FizzBuzz");
+        }
+        else if (i % 3 === 0 &&  i % 5 !== 0 ){
+            console.log("Fizz");
+        }
+        else if (i % 5 === 0 &&  i % 3 !== 0){
+            console.log("Buzz");
+        }
+        else if(i % 5 !== 0 && i % 3 !== 0){
+            console.log(i)
+        }
+        i++;
+    }
+}
+
+console.log(fizzBuzz(15))

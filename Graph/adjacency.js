776606a85e2476f52cdc87369f1541dff27c3b2f
Ellie Tahmasebi <59767576+ellietms@users.noreply.github.com>
2023-01-08 23:08:28 +0000

    added graph classe

---
+++ Graph/adjacency.js
@@ -0,0 +1,22 @@
+
+class Graph {
+    constructor( ){
+        this.myAdjacency = {};
+    }
+
+    addVertex(nodeName){
+       if(!this.myAdjacency[nodeName]) this.myAdjacency[nodeName] = []
+    }
+}
+
+
+let firstGraph = new Graph()
+
+firstGraph.addVertex("Tokyo")
+firstGraph.addVertex("Thailand")
+
+
+firstGraph
+
+
+console.log(firstGraph)

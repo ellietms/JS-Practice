a90273515b0c2ea66e3951ec4d4b25ce757e2332
Ellie Tahmasebi <59767576+ellietms@users.noreply.github.com>
2023-01-11 23:27:56 +0000

    added remove vertex function for my graph

--- Graph/adjacency.js
+++ Graph/adjacency.js
@@ -17,40 +17,63 @@ class Graph {
     }
   }
 
-  getList(){
-    return this.myAdjacency
+  removeEdge(vertext1, vertext2) {
+    if (this.myAdjacency[vertext1].includes(vertext2)) {
+      this.myAdjacency[vertext1] = this.myAdjacency[vertext1].filter(
+        (vertext) => vertext !== vertext2
+      );
+    }
+
+    if (this.myAdjacency[vertext2].includes(vertext1)) {
+      this.myAdjacency[vertext2] = this.myAdjacency[vertext2].filter(
+        (vertext) => vertext !== vertext1
+      );
+    }
+  }
+
+  removeVertex(vertextName) {
+    this.myAdjacency[vertextName].map((eachEdge) =>
+      this.removeEdge(eachEdge, vertextName)
+    );
+
+    delete this.myAdjacency[vertextName]
+  }
+
+  getList() {
+    return this.myAdjacency;
   }
 }
 
+// Try the class
+
+// first class
 let firstGraph = new Graph();
 
 firstGraph.addVertex("Tokyo");
 firstGraph.addVertex("Thailand");
+firstGraph.addEdge("Tokyo", "Thailand");
 
 console.log(firstGraph);
 
-firstGraph.addEdge("Tokyo", "Thailand")
+// second class
 
-console.log(firstGraph);
+let secondGraph = new Graph();
 
-let secondGraph = new Graph()
+// add vertext
+secondGraph.addVertex("shiraz");
+secondGraph.addVertex("Esfahan");
+secondGraph.addVertex("Tehran");
 
-secondGraph.addVertex("shiraz")
-secondGraph.addVertex("Esfahan")
-secondGraph.addVertex("Tehran")
+// add edge
+secondGraph.addEdge("shiraz", "Tehran");
+secondGraph.addEdge("Tehran", "Esfahan");
+secondGraph.addEdge("shiraz", "Esfahan");
 
-console.log(secondGraph)
+console.log(secondGraph);
 
-secondGraph.addEdge("shiraz", "Tehran")
+// remove edge
+// secondGraph.removeEdge("shiraz", "Tehran");
+// console.log("This ========> ", secondGraph);
 
-console.log(secondGraph)
-
-
-secondGraph.addEdge("Tehran", "Esfahan")
-
-console.log(secondGraph)
-
-
-secondGraph.addEdge("shiraz", "Esfahan")
-
-console.log(secondGraph)
+secondGraph.removeVertex("Esfahan")
+console.log("=======> ",secondGraph)
